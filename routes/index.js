3b7c8d40821f5b6c40ba04fd009594a3fd7e9753
Rajeshkanna <v.rajeshkanna@gmail.com>
2022-09-28 18:15:44 +0530

    💬 Update to 2022

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TNEA 2016' });
+  res.render('index', { title: 'TNEA 2022' });
 });
 
 module.exports = router;
