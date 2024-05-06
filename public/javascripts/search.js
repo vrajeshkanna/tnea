3f436f2b4893a626842ed643f71a27cd11fefb4a
Rajeshkanna <v.rajeshkanna@gmail.com>
2024-05-06 12:26:55 -0400

    Update 2023 data

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -2,61 +2,58 @@
 var districtListData = [];
 
 // DOM Ready =============================================================
-$(document).ready(function() {
+$(document).ready(function () {
+  // Populate the user table on initial page load
+  var table = populateTable();
 
-    // Populate the user table on initial page load
-    var table = populateTable();
-
-    // Event listener to the two range filtering inputs to redraw on input
-   $('#min, #max').keyup( function() {
-     $('#colleges_table').DataTable().draw();
-   } );
+  // Event listener to the two range filtering inputs to redraw on input
+  $("#min, #max").keyup(function () {
+    $("#colleges_table").DataTable().draw();
+  });
 });
 
 // Functions =============================================================
 
 // Fill table with data
 function populateTable() {
+  var collegeTableContent = "";
 
-    var collegeTableContent = '';
-
-    $.getJSON( '/data/2022.json', function( data ) {
-        // For each item in our JSON, add a table row and cells to the content string
-        $.each(data, function(){
-            collegeTableContent += '<tr>';
-            collegeTableContent += '<td>' + this.BC + '</td>';
-            collegeTableContent += '<td>' + this.OC + '</td>';
-            collegeTableContent += '<td>' + this.MBC + '</td>';
-            collegeTableContent += '<td>' + this.SC + '</td>';
-            collegeTableContent += '<td>' + this.ST + '</td>';
-            collegeTableContent += '<td>' + this.brn + '</td>';
-            collegeTableContent += '<td>' + this.con + '</td>';
-            collegeTableContent += '<td>' + this.coc + '</td>';
-            collegeTableContent += '</tr>';
-        });
-
-        // Inject the whole content string into our existing HTML table
-        $('#collegeList table tbody').html(collegeTableContent);
-        return $('#colleges_table').DataTable({
-        "order": [[ 0, "desc" ]]
+  $.getJSON("/data/2023.json", function (data) {
+    // For each item in our JSON, add a table row and cells to the content string
+    $.each(data, function () {
+      collegeTableContent += "<tr>";
+      collegeTableContent += "<td>" + this.BC + "</td>";
+      collegeTableContent += "<td>" + this.OC + "</td>";
+      collegeTableContent += "<td>" + this.MBC + "</td>";
+      collegeTableContent += "<td>" + this.SC + "</td>";
+      collegeTableContent += "<td>" + this.ST + "</td>";
+      collegeTableContent += "<td>" + this.brn + "</td>";
+      collegeTableContent += "<td>" + this.con + "</td>";
+      collegeTableContent += "<td>" + this.coc + "</td>";
+      collegeTableContent += "</tr>";
     });
+
+    // Inject the whole content string into our existing HTML table
+    $("#collegeList table tbody").html(collegeTableContent);
+    return $("#colleges_table").DataTable({
+      order: [[0, "desc"]],
     });
-};
+  });
+}
 
 /* Custom filtering function which will search data in column four between two values */
-$.fn.dataTable.ext.search.push(
-    function( settings, data, dataIndex ) {
-        var min = parseInt( $('#min').val(), 10 );
-        var max = parseInt( $('#max').val(), 10 );
-        var cutoff = parseFloat( data[0] ) || 0; // use data for the cutoff column
+$.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+  var min = parseInt($("#min").val(), 10);
+  var max = parseInt($("#max").val(), 10);
+  var cutoff = parseFloat(data[0]) || 0; // use data for the cutoff column
 
-        if ( ( isNaN( min ) && isNaN( max ) ) ||
-             ( isNaN( min ) && cutoff <= max ) ||
-             ( min <= cutoff   && isNaN( max ) ) ||
-             ( min <= cutoff   && cutoff <= max ) )
-        {
-            return true;
-        }
-        return false;
-    }
-);
+  if (
+    (isNaN(min) && isNaN(max)) ||
+    (isNaN(min) && cutoff <= max) ||
+    (min <= cutoff && isNaN(max)) ||
+    (min <= cutoff && cutoff <= max)
+  ) {
+    return true;
+  }
+  return false;
+});
